1e57d3ebd5f4c5c406f964ea527f54c5c71c7018
cryy22 <116713472+cryy22@users.noreply.github.com>
2024-10-24 19:28:09 -0700

    wip: click to show/hide experiment notes

--- Assets/Scripts/ExperimentNotes.cs
+++ Assets/Scripts/ExperimentNotes.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,8 +8,51 @@ namespace GodsExperiment
     {
         [SerializeField] private Transform NotesCard;
         [SerializeField] private Button ShowHideButton;
+        [SerializeField] private TMP_Text ShowHideButtonText;
+        [SerializeField] private float ShowHideDuration = 0.5f;
 
         [SerializeField] private Vector2 OffscreenPoint;
         [SerializeField] private Vector2 OnscreenPoint;
+
+        private bool _isShown;
+        private bool _isMoving;
+        private Vector3 _destination = Vector3.zero;
+        private Vector3 _startPosition = Vector3.zero;
+        private float _progress;
+
+        private void Awake() { NotesCard.localPosition = OffscreenPoint; }
+
+        private void Update()
+        {
+            if (!_isMoving) return;
+
+            _progress += Time.deltaTime / ShowHideDuration;
+            NotesCard.localPosition = Vector3.Lerp(a: _startPosition, b: _destination, t: _progress);
+            if (_progress >= 1)
+                _isMoving = false;
+        }
+
+        private void OnEnable() { ShowHideButton.onClick.AddListener(OnShowHideButtonClicked); }
+        private void OnDisable() { ShowHideButton.onClick.RemoveListener(OnShowHideButtonClicked); }
+
+        private void OnShowHideButtonClicked()
+        {
+            if (!_isShown)
+            {
+                _isShown = true;
+                _destination = OnscreenPoint;
+                ShowHideButtonText.text = "hide experiment notes";
+            }
+            else
+            {
+                _isShown = false;
+                _destination = OffscreenPoint;
+                ShowHideButtonText.text = "show experiment notes";
+            }
+
+            _isMoving = true;
+            _progress = 0f;
+            _startPosition = NotesCard.localPosition;
+        }
     }
 }
