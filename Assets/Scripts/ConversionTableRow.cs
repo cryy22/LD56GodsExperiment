27a1c84b6b4731eb288d6e5c1b82eb61fa143cd1
cryy22 <116713472+cryy22@users.noreply.github.com>
2024-11-09 08:08:07 -0800

    conversion table should show two reagents

--- Assets/Scripts/ConversionTableRow.cs
+++ Assets/Scripts/ConversionTableRow.cs
@@ -7,8 +7,10 @@ namespace GodsExperiment
     public class ConversionTableRow : MonoBehaviour
     {
         [SerializeField] private Image TargetResourceIcon;
-        [SerializeField] private Image SourceResourceIcon;
-        [SerializeField] private TMP_Text SourceResourceCountText;
+        [SerializeField] private Image SourceResourceIcon1;
+        [SerializeField] private TMP_Text SourceResourceCountText1;
+        [SerializeField] private Image SourceResourceIcon2;
+        [SerializeField] private TMP_Text SourceResourceCountText2;
         [SerializeField] private TMP_Text UnitsPerDayText;
 
         public void SetResourceCosts(
@@ -19,18 +21,36 @@ namespace GodsExperiment
         {
             TargetResourceIcon.sprite = ResourceDefinitionIndex.I.GetSpriteForResource(resourceType);
 
-            if (resources[resourceType].ResourceCosts.Count > 0)
+            SourceResourceIcon1.gameObject.SetActive(false);
+            SourceResourceCountText1.gameObject.SetActive(false);
+            SourceResourceIcon2.gameObject.SetActive(false);
+            SourceResourceCountText2.gameObject.SetActive(false);
+
+            var sourceResourceIndex = 1;
+            foreach ((ResourceType sourceResourceType, float cost) in resources[resourceType].ResourceCosts)
             {
-                foreach ((ResourceType sourceResourceType, float cost) in resources[resourceType].ResourceCosts)
+                if (sourceResourceIndex > 2)
+                    break;
+
+                Image icon = sourceResourceIndex switch
                 {
-                    SourceResourceIcon.sprite = ResourceDefinitionIndex.I.GetSpriteForResource(sourceResourceType);
-                    SourceResourceCountText.text = $"{(int) cost}";
-                }
-            }
-            else
-            {
-                SourceResourceIcon.gameObject.SetActive(false);
-                SourceResourceCountText.gameObject.SetActive(false);
+                    1 => SourceResourceIcon1,
+                    _ => SourceResourceIcon2,
+                };
+
+                TMP_Text text = sourceResourceIndex switch
+                {
+                    1 => SourceResourceCountText1,
+                    _ => SourceResourceCountText2,
+                };
+
+                icon.gameObject.SetActive(true);
+                icon.sprite = ResourceDefinitionIndex.I.GetSpriteForResource(sourceResourceType);
+
+                text.gameObject.SetActive(true);
+                text.text = $"{(int) cost}";
+
+                sourceResourceIndex++;
             }
 
             float unitsPerDay = config.TimePerDay / resources[resourceType].WorkUnitsPerUnit;
